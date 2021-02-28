c8ee7e8b5b85a23b41a7799872840df4c49c0d36
milkahailu <56700121+milkahailu@users.noreply.github.com>
2021-02-28 23:47:05 +0300

    Add files via upload

--- app/lib/user/repository/user_repository.dart
+++ app/lib/user/repository/user_repository.dart
@@ -1,26 +1,45 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:delala/models/errors.dart';
 import 'package:delala/user/data_provider/user_data.dart';
 import 'package:delala/user/models/user.dart';
+import 'package:delala/user/repository/user_repository_response.dart';
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
 
 class UserRepository {
-  final UserDataProvider userProvider;
+  final UserDataProvider dataProvider;
 
-  UserRepository({@required this.userProvider}) : assert(userProvider != null);
+  UserRepository({@required this.dataProvider}) : assert(dataProvider != null);
 
-  Future<Response> createUser(User user) async {
-    return await userProvider.createUser(user);
+  Future<UserRepositoryResponse> initCreateUser(User user) async {
+    Response response = await dataProvider.initCreateUser(user);
+
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        return RUserCreateInitSuccess(response.statusCode, user);
+      case HttpStatus.badRequest:
+        var jsonData = json.decode(response.body);
+        return RUserCreateInitFailure(response.statusCode, jsonData);
+      case HttpStatus.internalServerError:
+        return RUserCreateInitFailure(
+            response.statusCode, {"error": FailedOperationError});
+      default:
+        return RUserCreateInitFailure(
+            response.statusCode, {"error": SomethingWentWrongError});
+    }
   }
 
   Future<User> getUser() async {
-    return await userProvider.getUser();
+    return await dataProvider.getUser();
   }
 
   Future<void> updateUser(User user) async {
-    await userProvider.updateUser(user);
+    await dataProvider.updateUser(user);
   }
 
   Future<void> deleteUser(String id) async {
-    await userProvider.deleteUser(id);
+    await dataProvider.deleteUser(id);
   }
 }
