970af99d172b8bc852662e0ac968f20d373a72ab
milkahailu <56700121+milkahailu@users.noreply.github.com>
2021-02-28 23:36:20 +0300

    Add files via upload

--- app/lib/user/data_provider/user_data.dart
+++ app/lib/user/data_provider/user_data.dart
@@ -1,35 +1,30 @@
 import 'dart:convert';
 
+import 'package:delala/config/constants.dart';
 import 'package:delala/user/models/user.dart';
 import 'package:delala/utils/request.maker.dart';
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 
 class UserDataProvider {
-  final _baseUrl = 'http://192.168.56.1:3000';
   final http.Client httpClient;
 
   UserDataProvider({@required this.httpClient}) : assert(httpClient != null);
 
-  Future<http.Response> createUser(User user) async {
-    var requester = HttpRequester(path: "/oauth/user/register/init");
-    try {
-      var response =
-          await http.post(requester.requestURL, headers: <String, String>{
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }, body: <String, String>{
-        'user_name': user.userName,
-        'phone_number': user.phoneNumber,
-      });
+  Future<http.Response> initCreateUser(User user) async {
+    var requester = HttpRequester(path: "/oauth/user/register");
+    // var response = await http.post(requester.requestURL,
+    //     headers: <String, String>{
+    //       'Content-Type': 'application/x-www-form-urlencoded',
+    //     },
+    //     body: json.encode(user.toJson()));
 
-      return response;
-    } catch (e) {
-      throw (e);
-    }
+    var response = http.Response("Hello", 200);
+    return response;
   }
 
   Future<User> getUser() async {
-    final response = await httpClient.get('$_baseUrl/courses');
+    final response = await httpClient.get('$BaseURI/courses');
 
     if (response.statusCode == 200) {
       final user = jsonDecode(response.body) as User;
@@ -41,7 +36,7 @@ class UserDataProvider {
 
   Future<void> deleteUser(String id) async {
     final http.Response response = await httpClient.delete(
-      '$_baseUrl/courses/$id',
+      '$BaseURI/courses/$id',
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -54,16 +49,11 @@ class UserDataProvider {
 
   Future<void> updateUser(User user) async {
     final http.Response response = await httpClient.put(
-      '$_baseUrl/courses/${user.id}',
+      '$BaseURI/courses/${user.id}',
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: jsonEncode(<String, dynamic>{
-        'id': user.id,
-        'user_name': user.userName,
-        'phone_number': user.phoneNumber,
-        'category': user.category,
-      }),
+      body: jsonEncode(user.toJson()),
     );
 
     if (response.statusCode != 204) {
